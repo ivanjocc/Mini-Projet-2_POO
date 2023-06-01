d19930958fe8100306151a0d6a294014a38b32c5
Iván <ivanjose_04@hotmail.com>
2023-05-31 21:57:35 -0400

    first commit

---
+++ Project2/CompteBancaire.cs
@@ -0,0 +1,46 @@
+﻿public class CompteBancaire
+{
+    private int numeroCompte;
+    private string nom;
+    private double solde;
+
+    public CompteBancaire(int numeroCompte, string nom, double solde)
+    {
+        this.numeroCompte = numeroCompte;
+        this.nom = nom;
+        this.solde = solde;
+    }
+
+    public void Versement(double montant)
+    {
+        solde += montant;
+        Console.WriteLine("tu as fait un versement");
+    }
+
+    public void Retrait(double montant)
+    {
+        if (solde >= montant)
+        {
+            solde -= montant;
+            Console.WriteLine("tu as fait un retrait");
+        }
+        else
+        {
+            Console.WriteLine("tu as fait un retrait et Il n'y a pas argent :)");
+        }
+    }
+
+    public void Agios()
+    {
+        double agios = solde * 0.05;
+        solde -= agios;
+        Console.WriteLine("tu as fait un agios");
+    }
+
+    public void Afficher()
+    {
+        Console.WriteLine("Le nom de la personne est: {0}", nom);
+        Console.WriteLine("Le numero de compte est: {0}", numeroCompte);
+        Console.WriteLine("Le solde est: {0}", solde);
+    }
+}
---
+++ Project2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Project2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CompteBancaire newObject = new CompteBancaire(123123123, "tigro feliz", 10000);
+
+            newObject.Afficher();
+            Console.WriteLine();
+            
+            newObject.Versement(1000);
+            newObject.Afficher();
+            Console.WriteLine();
+
+            newObject.Retrait(20000);
+            newObject.Afficher();
+            Console.WriteLine();
+
+            newObject.Agios();
+            newObject.Afficher();
+        }
+    }
+}
